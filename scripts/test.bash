683a32f68a33d50d3f5e2170fa013027e0acd550
BraeTroutman <braetroutman@gmail.com>
2023-02-14 08:53:20 -0500

    don't generate matrices with 0 length in any dimension

--- scripts/test.bash
+++ scripts/test.bash
@@ -2,9 +2,9 @@
 
 for i in {1..100}
 do
-	M=$((RANDOM%100))
-	N=$((RANDOM%100))
-	K=$((RANDOM%100))
+	M=$((RANDOM%99 + 1))
+	N=$((RANDOM%99 + 1))
+	K=$((RANDOM%99 + 1))
 	RES=$(./bin/matmul $M $N $K)
 
 	if [ -z $RES ]
