aedcc93420ee3d39cf570b8e53d7b81582ca06e8
BraeTroutman <braetroutman@gmail.com>
2023-02-14 11:13:02 -0500

    remove parse error handling from main

--- src/main.cu
+++ src/main.cu
@@ -12,34 +12,26 @@ int main(int argc, char **argv)
 {	
 	options_t opts = parse_args(argc, argv);
 	
-	int optslen;
-	for (optslen = 0; opts.remaining[optslen] != NULL; ++optslen);
-
-	if (optslen < 3) {
-		printf("Usage: %s [options] M N K\n", argv[0]);	
-		return 1;
-	}
-	
-	int M = atoi(opts.remaining[0]);
-	int N = atoi(opts.remaining[1]);
-	int K = atoi(opts.remaining[2]);
-
-	vector<int> A = matrixAlloc(M,N);
-	vector<int> B = matrixAlloc(N,K);	
-	vector<int> C = cudaMatmul(A, B, M, N, K);
+	vector<int> A = matrixAlloc(opts.M,opts.N);
+	vector<int> B = matrixAlloc(opts.N,opts.K);	
+	vector<int> C = cudaMatmul(A, B, opts.M, opts.N, opts.K);
 	
 	if (opts.verbose) {
 		cout << "A" << endl;
-		printMat(A, M, N);
+		printMat(A, opts.M, opts.N);
 		cout << "B" << endl;
-		printMat(B, N, K);
+		printMat(B, opts.N, opts.K);
 		cout << "C" << endl;
-		printMat(C, M, K);
+		printMat(C, opts.M, opts.K);
 	}
 
 	if (opts.check) {
-		vector<int> checkC = seqMatmul(A, B, M, N, K);
-		if (C != checkC) printMat(C, checkC, M, K);
+		vector<int> checkC = seqMatmul(A, B, opts.M, opts.N, opts.K);
+		if (C != checkC) {
+			printMat(C, checkC, opts.M, opts.K);
+		} else {
+			puts("success!");
+		}
 	}
 }
 
